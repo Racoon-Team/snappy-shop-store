58d360ab4911f7f16a308b89fa13250b4c1ecd1d
Fernando Ricaldes <fernandorashed258@gmail.com>
2025-11-30 10:41:35 -0400

    FIX #34 payment-method style fix

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -230,7 +230,7 @@ const Checkout = () => {
                         <CardElement /> <p className="text-red-400 text-sm mt-1">{error}</p>
                       </div>
                     )}
-                    <div className="grid sm:grid-cols-3 grid-cols-1 gap-4">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                       {storeSetting?.cod_status && (
                         <div className="">
                           <InputPayment
